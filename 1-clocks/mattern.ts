0074c0a1b7cc7132b0563770f91690be1e99d4a2
Hououin Kyouma <inwazzin@gmail.com>
2022-09-16 20:59:41 +0200

    mattern clock algorithm

---
+++ 1-clocks/mattern.ts
@@ -0,0 +1,27 @@
+import { addListeners, deliver, Frame, Internal, VectorClock, Message, Receive, Send, send } from "../definitions";
+
+interface Packet extends Frame {
+  clock: VectorClock;
+  message: Message,
+}
+interface Process {
+  id: number;
+  clock: VectorClock;
+  monitor: Monitor;
+}
+interface Monitor {
+  process: Process;
+}
+
+addListeners([
+  [Receive, (sender: Monitor, destination: Monitor, packet: Packet) => {
+    destination.process.clock.tickAt(destination.process.id);
+    destination.process.clock.sync(packet.clock);
+    deliver(sender.process, destination.process, packet.message);
+  }],
+  [Send, (sender: Process, destination: Process, message: Message) => {
+    sender.clock.tickAt(sender.id);
+    send(sender.monitor, destination.monitor, { clock: sender.clock, message });
+  }],
+  [Internal, (self: Process) => self.clock.tickAt(self.id)],
+]);
