9ec50394c9930a6dc99e291d9d4831848bfdcde3
Hououin Kyouma <inwazzin@gmail.com>
2022-09-16 21:00:05 +0200

    birman schiper stephenson

---
+++ 2-channels/birman-schiper-stephenson.ts
@@ -0,0 +1,36 @@
+import { addListeners, deliver, Message, Receive, Send, send, VectorClock } from "../definitions";
+import { processes } from "../globals";
+import { omit, waitfor } from "../utils";
+
+// Gwarancja kolejności dostarczenia wiadomości w sieciach monitorów poprzez każdorazowe
+// rozgłaszanie wektora kolejności zdarzeń każdego z procesów.
+
+interface Packet {
+  clock: VectorClock;
+  message: Message;
+}
+interface Process {
+  clock: VectorClock;
+  id: number;
+}
+interface Monitor {
+  process: Process;
+}
+
+addListeners([
+  [Send, (sender: Process, destination: Process, message: Message) => {
+    sender.clock.tickAt(sender.id);
+    send(sender, omit(processes, sender), { clocks: sender.clock, message });
+  }],
+  [Receive, async (sender: Monitor, destination: Monitor, { message, clock }: Packet) => {
+    await waitfor(() =>
+      destination.process[sender.process.id] === clock[sender.process.id] - 1 &&
+      omit(processes, sender).every(({ clock }) => destination.process.clock.isLaterOrSameAs(clock)),
+    );
+
+    if (message.destinationId === destination.process.id)
+      deliver(sender.process, destination.process, message);
+
+    destination.process.clock.sync(clock);
+  }],
+]);
